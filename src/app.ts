9a24befe4f5f08638ac9e579852bbb72d92e056f
rodrigosuelli <rodrigosuelli@gmail.com>
2024-03-07 17:28:56 -0300

    install @types/node

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,10 @@
+import fs from 'fs';
+
+fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+
 let n1: number = 1;
 const hi = 'hi';
 
@@ -10,4 +17,4 @@ function sum(a: number, b: number) {
   return a + b;
 }
 
-console.log(sum('1', 2));
+console.log(sum(1, 2));
